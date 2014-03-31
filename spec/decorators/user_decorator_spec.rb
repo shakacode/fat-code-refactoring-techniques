f3062696478c82ab1d5b1761dc0cd6b0d8fe2148
Justin Gordon <justin.gordon@gmail.com>
2014-03-30 18:43:16 -1000

    Improved test speed by consolidating 2 slow tests.
    Rather than create the 1000 users twice...Do it once.

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -7,24 +7,12 @@ describe UserDecorator do
       1000.times do
         other_user = FactoryGirl.create(:user)
         @user.follow!(other_user)
-      end
-    end
-
-    it "returns a number with a comma" do
-      expect(@user.decorate.formatted_followed_count).to eq("1,000")
-    end
-  end
-
-  describe "#formatted_followers_count" do
-    before do
-      @user = FactoryGirl.create(:user)
-      1000.times do
-        other_user = FactoryGirl.create(:user)
         other_user.follow!(@user)
       end
     end
 
     it "returns a number with a comma" do
+      expect(@user.decorate.formatted_followed_count).to eq("1,000")
       expect(@user.decorate.formatted_followers_count).to eq("1,000")
     end
   end
