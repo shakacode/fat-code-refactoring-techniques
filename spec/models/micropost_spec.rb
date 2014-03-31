2f5eeae0aadcd26aef3bfd22fdfc49d5b9f83e34
Justin Gordon <justin.gordon@gmail.com>
2014-03-30 15:36:59 -1000

    Annotated models

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer          not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe Micropost do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime
+#  updated_at      :datetime
+#  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean
+#
+
 require 'spec_helper'
 
 describe User do
