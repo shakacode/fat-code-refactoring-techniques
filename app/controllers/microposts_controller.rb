342e7b2708314936ae676d454db96402eb4c9452
Justin Gordon <justin.gordon@gmail.com>
2014-04-05 12:20:10 -1000

    Fat controller handling of profanity in minor posts
    * Check post for profanity if current_user is a minor.
    * Display validation error.
    * Increment counter for profanity on minor.
    * Send email to parents.
    * Too much logic in controller method create.
    * Thorough controller spec example.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,15 +1,25 @@
 class MicropostsController < ApplicationController
   before_action :signed_in_user, only: [:create, :destroy]
-  before_action :correct_user,   only: :destroy
+  before_action :correct_user, only: :destroy
 
   def create
-    @micropost = current_user.microposts.build(micropost_params)
-    if @micropost.save
-      flash[:success] = "Micropost created!"
-      redirect_to root_url
+    @micropost = Micropost.new(micropost_params.merge(user: current_user))
+    if current_user.minor? && (profane_words_used = profane_words_in(@micropost.content))
+        current_user.increment(:profanity_count, profane_words_used.size)
+        current_user.save(validate: false)
+        send_parent_notifcation_of_profanity(profane_words_used)
+        flash.now[:error] = <<-MSG
+              Whoa, better watch your language! Profanity: '#{profane_words_used.join(", ")}' not allowed!
+              You've tried to use profanity #{view_context.pluralize(current_user.profanity_count, "time")}!
+        MSG
+        render 'static_pages/home'
     else
-      @feed_items = []
-      render 'static_pages/home'
+      if @micropost.save
+        flash[:success] = "Micropost created!"
+        redirect_to root_url
+      else
+        render 'static_pages/home'
+      end
     end
   end
 
@@ -19,6 +29,22 @@ class MicropostsController < ApplicationController
   end
 
   private
+
+    # return array of profane words in content or nil if none
+    def profane_words_in(content)
+      # PRETEND: Hit external REST API
+
+      # NOTE: Implementation below is a simulation
+      profane_words = %w(poop fart fartface poopface poopbuttface)
+      content_words = content.split(/\W/)
+      content_words.select { |word| word.in? profane_words }.presence
+    end
+
+    def send_parent_notifcation_of_profanity(profane_words)
+      # PRETEND: send email
+      Rails.logger.info("Sent profanity alert email to parent of #{current_user.name}, "\
+          "who used profane words: #{profane_words}")
+    end
 
     def micropost_params
       params.require(:micropost).permit(:content)
