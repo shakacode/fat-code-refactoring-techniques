56608aa3d64c74aa8a3a68e4dd849ce9095a8ed0
Justin Gordon <justin.gordon@gmail.com>
2014-04-05 13:02:22 -1000

    Break out profanity checking into own service
    * Clarified and simplified tests.
    * Left in 2 broken tests regarding the profanity counter.

--- app/services/micropost_creation_service.rb
+++ app/services/micropost_creation_service.rb
@@ -26,37 +26,34 @@ class MicropostCreationService
     end
   end
 
-
-  # return response or
+  # return response if content has profanity words and adds the number of profanity words to the
+  # user's profanity counter
   def check_profanity(micropost)
-    if profanity_word
-      msg = "Whoa, better watch your language! Profanity: '#{profanity_word}' not allowed!"
-      @user.increment(:profanity_count)
+    return nil unless @user.minor?
+
+    if has_profanity?
+      @user.increment(:profanity_count, profanity_words.size)
+      msg = <<-MSG
+        Whoa, better watch your language! Profanity: '#{profanity_words.join(", ")}' not allowed!
+        You've tried to use profanity #{h.pluralize(@user.profanity_count, "time")}!
+      MSG
       ControllerResponse.new(flash_message: msg,
                              flash_type:    :error,
-                             flash_now:     true, # True b/c not redirecting
+                             flash_now:     true,
                              http_status:   :bad_request,
                              data:          { micropost: micropost })
     end
   end
 
-  PROFANITY_WORDS = %w(poop fart fartface poopface)
-
-  def profanity_word
-    @profanity_word ||= find_profanity_word
+  def has_profanity?
+    profanity_words.present?
   end
 
   # Yes, this could go into a validator for the micropost, but let's suppose there's reasons
-  # that we don't want to do that, such as we only want to filter profanity for posts
-  # created by minors, etc.
-  # returns profanity word if existing in content, or else nil
-  def find_profanity_word
-    return nil unless @user.minor?
-
-    words = @content.split(/\W/)
-    PROFANITY_WORDS.each do |test_word|
-      return test_word if words.include?(test_word)
-    end
-    nil
+  # that we don't want to do that, such as we want a special flash message.
+  # returns profanity words if existing in content, or else nil
+  def profanity_words
+    # Important not to memorize nil or false
+    @profanity_words ||= ProfanityChecker.new(@content).profanity_words_contained
   end
 end
--- spec/services/micropost_creation_service_spec.rb
+++ spec/services/micropost_creation_service_spec.rb
@@ -1,57 +1,48 @@
 describe MicropostCreationService do
-  let(:minor) { FactoryGirl.create :user, minor: true }
-  let(:adult) { FactoryGirl.create :user, minor: false }
+  let(:minor) { FactoryGirl.create :user, minor: true, profanity_count: 0 }
+  let(:adult) { FactoryGirl.create :user, minor: false, profanity_count: 0 }
 
-  describe "#profanity_word" do
+  describe "#create_micropost" do
     let(:profanity_content) { "Dad is a poopface!" }
     context "minor" do
-      subject(:service) { MicropostCreationService.new(minor, profanity_content) }
-      context "does contain profanity" do
-        describe "#profanity_word" do
-          it "returns poopface" do
-            expect(service.profanity_word).to eq("poopface")
-          end
+      context "does contain one word profanity" do
+        subject(:service) { MicropostCreationService.new(minor, profanity_content) }
+        it "returns a controller response" do
+          response = service.create_micropost
+          expect(response.flash_message).to match /poopface/
+          expect(response.flash_type).to eq(:error)
+          expect(response.ok).to be_false
+          expect(minor.reload.profanity_count).to eq(1)
         end
+      end
 
-        describe "#create" do
-          it "returns a controller response" do
-            response = service.create_micropost
-            expect(response.flash_message).to match /poopface/
-            expect(response.flash_type).to eq(:error)
-            expect(response.ok).to be_false
-          end
+      context "does contain two word profanity" do
+        subject(:service) { MicropostCreationService.new(minor, "Dad is a poop and poopbuttface") }
+        it "returns a controller response" do
+          response = service.create_micropost
+          expect(response.flash_message).to match /poopbuttface/
+          expect(response.flash_type).to eq(:error)
+          expect(response.ok).to be_false
+          expect(minor.reload.profanity_count).to eq(2)
         end
       end
 
       context "adult" do
         subject(:service) { MicropostCreationService.new(adult, profanity_content) }
         context "does contain profanity" do
-          describe "#profanity_word" do
-            it "returns nil" do
-              expect(service.profanity_word).to be_nil
-            end
-          end
-
-          describe "#create" do
-            it "returns a controller response" do
-              response = service.create_micropost
-              expect(response.flash_message).to eq("Micropost created!")
-              expect(response.flash_type).to eq(:success)
-              expect(response.redirect_path).to_not be_nil
-              expect(response.ok).to be_true
-            end
+          it "returns a controller response" do
+            response = service.create_micropost
+            expect(response.flash_message).to eq("Micropost created!")
+            expect(response.flash_type).to eq(:success)
+            expect(response.redirect_path).to_not be_nil
+            expect(response.ok).to be_true
+            expect(adult.profanity_count).to eq(0)
           end
         end
       end
 
       context "content does not contain profanity" do
         subject(:service) { MicropostCreationService.new(minor, "Dad is the best!") }
-
-        describe "#profanity_word" do
-          it "returns nil" do
-            expect(service.profanity_word).to be_nil
-          end
-        end
 
         describe "#create" do
           it "returns a controller response" do
@@ -60,6 +51,7 @@ describe MicropostCreationService do
             expect(response.flash_type).to eq(:success)
             expect(response.redirect_path).to_not be_nil
             expect(response.ok).to be_true
+            expect(minor.profanity_count).to eq(0)
           end
         end
       end
