525396c931733601f178e0cf7043f94411d519d4
Justin Gordon <justin.gordon@gmail.com>
2014-04-05 21:34:27 -1000

    Add profanity validation to Micropost
    * Example of using the ProfanityChecker service in multiple places.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -14,12 +14,21 @@ class Micropost < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
-
+  validate :profanity_checker
   # Returns microposts from the users being followed by the given user.
   def self.from_users_followed_by(user)
     followed_user_ids = "SELECT followed_id FROM relationships
                          WHERE follower_id = :user_id"
-    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
           user_id: user.id)
+  end
+
+  private
+
+  def profanity_checker
+    return unless user && user.minor?
+
+    profanity_words = ProfanityChecker.new(content).profanity_words_contained
+    errors.add(:content, "Profanity: '#{profanity_words.join(", ")}' not allowed!") if profanity_words.present?
   end
 end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -18,7 +18,7 @@ describe Micropost do
     # This code is not idiomatically correct.
     @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
   end
-  
+
   subject { @micropost }
 
   it { should respond_to(:content) }
@@ -28,14 +28,30 @@ describe Micropost do
     before { @micropost.user_id = nil }
     it { should_not be_valid }
   end
-    
+
   describe "with blank content" do
     before { @micropost.content = " " }
     it { should_not be_valid }
   end
-  
+
   describe "with content that is too long" do
     before { @micropost.content = "a" * 141 }
     it { should_not be_valid }
+  end
+end
+
+describe "Micropost with profanity" do
+  describe "with content that contains profanity" do
+    it "is not valid for minor" do
+      minor = FactoryGirl.create(:user, minor: true)
+      micropost = Micropost.new(content: "Lorem ipsum poop", user: minor)
+      expect(micropost).not_to be_valid
+    end
+
+    it "is not valid for non-minor" do
+      minor = FactoryGirl.create(:user, minor: false)
+      micropost = Micropost.new(content: "Lorem ipsum poop", user: minor)
+      expect(micropost).to be_valid
+    end
   end
 end
