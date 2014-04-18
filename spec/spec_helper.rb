88b3549f51601885c5ce3b030a180b0cce61bf79
Justin Gordon <justin.gordon@gmail.com>
2014-04-17 19:36:00 -1000

    Added Draper gem and configured for spork
    Per Draper instructions for spork, added
      require 'draper/test/rspec_integration'
    to spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
+  require 'draper/test/rspec_integration'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
