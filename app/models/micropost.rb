d1025dca91c2e1c475893f4b712fa87ed5886b04
Justin Gordon <justin.gordon@gmail.com>
2014-04-18 07:44:16 -1000

    Move profanity validation from controller to model
    * Profanity check is called before model saved for a couple reasons:
    1. If we save the micropost, that creates a validation message which
       would duplicate the profanity flash.
    2. A bit more work confirm that validation error on the content was
       profanity without checking the message.
    
    * Added validation test.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -14,6 +14,7 @@ class Micropost < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
+  validate :no_profanity
 
   # Returns microposts from the users being followed by the given user.
   def self.from_users_followed_by(user)
@@ -22,4 +23,23 @@ class Micropost < ActiveRecord::Base
     where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
           user_id: user.id)
   end
+
+  # return array of profane words in content or nil if none
+  def profane_words_in_content
+    # PRETEND: Hit external REST API
+
+    # NOTE: Implementation below is a simulation
+    profane_words = %w(poop fart fartface poopface poopbuttface)
+    content_words = content.split(/\W/)
+    content_words.select { |word| word.in? profane_words }.presence
+  end
+
+  private
+
+    def no_profanity
+      if user && user.minor? && (profane_words = profane_words_in_content)
+        errors.add(:content, "Profanity: #{profane_words.join(", ")} not allowed!")
+      end
+    end
+
 end
