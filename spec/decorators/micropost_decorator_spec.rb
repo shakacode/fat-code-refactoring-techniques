4341cc5430abdb419f54b247e3b4664d4d1578a8
Justin Gordon <justin.gordon@gmail.com>
2014-04-18 15:01:45 -1000

    Move profanity_violation_message to MicropostDecorator
    This slims up the MicropostController create action method.

--- spec/decorators/micropost_decorator_spec.rb
+++ spec/decorators/micropost_decorator_spec.rb
@@ -1,10 +1,24 @@
 require 'spec_helper'
 
 describe MicropostDecorator do
-  let(:user) { FactoryGirl.create(:user) }
 
-  it "prints time ago in words" do
-    micropost = Micropost.create(content: "Lorem ipsum", user_id: user.id)
-    expect(micropost.decorate.posted_ago).to match /Posted less than a minute ago./
+  describe "posted_ago" do
+    let(:user) { FactoryGirl.create(:user) }
+    it "prints time ago in words" do
+      micropost = Micropost.create(content: "Lorem ipsum", user_id: user.id)
+      expect(micropost.decorate.posted_ago).to match /Posted less than a minute ago./
+    end
+  end
+
+  describe "profanity_violation_msg" do
+    let(:user) { FactoryGirl.create(:user, profanity_count: 20) }
+    it "creates a message" do
+      micropost               = Micropost.create(content: "Lorem poopface, fartface", user_id: user.id)
+      profanity_violation_msg = micropost.decorate.profanity_violation_msg
+      expect(profanity_violation_msg).to match /poopface/
+      expect(profanity_violation_msg).to match /fartface/
+      expect(profanity_violation_msg).to match /20 times/
+      expect(profanity_violation_msg).to match /Your parents have been notified!/
+    end
   end
 end
