bd89cba6df2c124be1af1b9bfd8d6745ae460674
Justin Gordon <justin.gordon@gmail.com>
2014-04-18 15:01:45 -1000

    Move profanity_violation_message to MicropostDecorator
    This slims up the MicropostController create action method.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -10,11 +10,7 @@ class MicropostsController < ApplicationController
     else
       if @micropost.profanity_validation_error?
         @micropost.errors[:content].clear # remove the default validation message
-        flash.now[:error] = <<-MSG.html_safe
-          <p>Whoa, better watch your language! Profanity: '#{@micropost.profane_words_in_content.join(", ")}' not allowed!
-          You've tried to use profanity #{view_context.pluralize(current_user.profanity_count, "time")}!
-          </p><p class="parent-notification">Your parents have been notified!</p>
-        MSG
+        flash.now[:error] = @micropost.decorate.profanity_violation_msg
       end
       render 'static_pages/home'
     end
--- app/decorators/micropost_decorator.rb
+++ app/decorators/micropost_decorator.rb
@@ -5,4 +5,12 @@ class MicropostDecorator < Draper::Decorator
       "Posted #{h.time_ago_in_words(object.created_at)} ago."
     end
   end
+
+  def profanity_violation_msg
+    <<-MSG.html_safe
+      <p>Whoa, better watch your language! Profanity: '#{object.profane_words_in_content.join(", ")}' not allowed!
+      You've tried to use profanity #{h.pluralize(object.user.profanity_count, "time")}!
+      </p><p class="parent-notification">Your parents have been notified!</p>
+    MSG
+  end
 end
