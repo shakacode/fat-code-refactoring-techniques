c62a178d25c4a2ec55127924212d86889e26d39a
Justin Gordon <justin.gordon@gmail.com>
2014-04-19 12:52:40 -1000

    Break followers and following into separate controllers
    Example of how to split up a large controller.

---
+++ app/controllers/followers_controller.rb
@@ -0,0 +1,8 @@
+class FollowersController < ApplicationController
+  before_action :signed_in_user
+
+  def followers
+    @presenter = Users::FollowersPresenter.new(params[:id], params[:page])
+    render '/users/show_follow'
+  end
+end
---
+++ app/controllers/following_controller.rb
@@ -0,0 +1,8 @@
+class FollowingController < ApplicationController
+  before_action :signed_in_user
+
+  def following
+    @presenter = Users::FollowedUsersPresenter.new(params[:id], params[:page])
+    render '/users/show_follow'
+  end
+end
